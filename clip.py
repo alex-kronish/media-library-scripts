43dca1e59bcc755384fad1763649e098596881b4
Alex Kronish <55164789+alex-kronish@users.noreply.github.com>
2025-11-02 15:54:48 -0500

    you know how it is sometimes

---
+++ clip.py
@@ -0,0 +1,58 @@
+#!/tools/conda/bin/python
+import subprocess
+import logging
+import argparse
+import os
+import re
+import datetime
+# -- argparser and setup
+parser=argparse.ArgumentParser()
+parser.add_argument("--Filename", required=True)
+parser.add_argument("--Start",required=True)
+parser.add_argument("--End",required=True)
+args=parser.parse_args()
+filename=args.Filename
+start=args.Start
+end=args.End
+time_regex=r'\d\d:\d\d:\d\d(\.\d+)?'
+extn_regex=r'\.\w+$'
+start_validation=re.fullmatch(time_regex,start)
+end_validation=re.fullmatch(time_regex,end)
+# -- verify valid time formats
+if not start_validation:
+    print("Bad format for start time. XX:XX:XX or XX:XX:XX.XXX")
+    exit(99)
+if not end_validation:
+    print("Bad format for end time. XX:XX:XX or XX:XX:XX.XXX")
+    exit(99)
+# -- figure out if the source file exists
+if not filename.startswith('/'):
+    fully_qualified_source_file=os.path.join(os.getcwd(),filename)
+else:
+    fully_qualified_source_file=filename
+if not os.path.isfile(fully_qualified_source_file):
+    print(fully_qualified_source_file+" is not a file")
+    exit(99)
+print(fully_qualified_source_file)
+file_extension=re.search(extn_regex,filename).group(0)
+amt_to_truncate=-1*len(file_extension)
+target_filename=os.path.basename(filename)[:amt_to_truncate]+'.clipped.'+datetime.datetime.now().strftime("%Y%m%d.%H%M%S")+'.mp4'
+fully_qualified_target_file=os.path.join(os.getcwd(),target_filename)
+print(fully_qualified_target_file)
+command=['/usr/bin/ffmpeg', '-i', fully_qualified_source_file, '-vf', 'format=yuv420p',  '-metadata', 'title=',  '-c:v', 'h264_nvenc', '-c:a', 'ac3', '-y', '-map', '0:a', '-map','0:v', '-preset','slow', '-cq:v','30', '-rc:v','vbr', '-qmin', '0', '-b:v','0', '-max_muxing_queue_size', '1024', '-ss', start, '-to', end, fully_qualified_target_file]
+print(command)
+proc=subprocess.Popen(command,shell=False,stdout=subprocess.PIPE,stderr=subprocess.STDOUT,bufsize=1,universal_newlines=True)
+#proc_c=proc.communicate()
+while (exit_code:=proc.poll()) is None:
+    if proc.stdout:
+        ffmpeg_line=proc.stdout.readline()#.decode('utf-8')
+        if ffmpeg_line.strip() != '':
+            print(str(ffmpeg_line).strip())
+    if proc.stderr:
+        ffmpeg_line_stderr=proc.stderr.readline()#.decode('utf-8')
+        if ffmpeg_line_stderr.strip() !='':
+            print(str(ffmpeg_line_stderr).strip())
+exit_code=proc.returncode
+print("-----------------------------------------------------------------------------")
+print("ffmpeg exited with status code : "+str(exit_code))
+
